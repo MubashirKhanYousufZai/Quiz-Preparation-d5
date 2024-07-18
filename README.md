7e9c713c8c5b52cb5821744e649dcd93cf422853
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-07-19 03:28:37 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Quiz-Preparation-d5" 
---
+++ main.js
@@ -0,0 +1,10 @@
+// ASYNC FUNCTION: this function returns a promise. inside this function we can use await to atop the execution of the function.
+let color = async () => {
+    let trafficLight = ["Red", "Yellow", "Green"];
+    console.log(trafficLight);
+    console.log("On Red Light STOP🚦");
+    console.log("On Yellow Light RADDY🏍");
+    console.log("On Green Light GO🏎");
+};
+color();
+export {};
